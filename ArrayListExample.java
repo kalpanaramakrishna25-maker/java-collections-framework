f5f40d62054a518ab2a6fa53e3844043dde98811
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-08 06:40:01 +0530

    Add ArrayListExample to demonstrate ArrayList usage

---
+++ ArrayListExample.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+
+        System.out.println("Fruits: " + fruits);
+
+        fruits.remove("Banana");
+
+        System.out.println("After removing Banana: " + fruits);
+    }
+}
